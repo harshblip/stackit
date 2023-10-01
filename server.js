6406682765e6925782b9f22c2d2632f5eef1b3f6
harshblip <harsh.jipkate2020@vitbhopal.ac.in>
2023-10-01 14:24:38 +0530

    commit message

--- server.js
+++ server.js
@@ -12,45 +12,46 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 app.post('/upload', upload.single('csvFile'), (req, res) => {
-  if (!req.file) {
-    return res.status(400).send('No file uploaded.');
-  }
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
 
-  const fileBuffer = req.file.buffer.toString();
-  const results = [];
+    const fileBuffer = req.file.buffer;
 
-  // Process the CSV data (you can use csv-parser or other libraries for parsing)
-  csv({ headers: true })
-    .toString(fileBuffer)
-    .on('data', (data) => results.push(data))
-    .on('end', () => {
-      // At this point, `results` contains your CSV data as an array of objects
-      // You can process the data or save it to a database
-      const firstRow = results.length > 0 ? results[0] : {};
-      res.status(200).json(firstRow);
-    });
+    const results = [];
+
+    // Process the CSV data using csv-parser
+    csv({ headers: true })
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
+            const dataMatrix = results.map((row) => Object.values(row));
+            const firstRow = dataMatrix.length > 0 ? dataMatrix[0] : {};
+
+            res.status(200).json(firstRow);
+        })
+        .write(fileBuffer);
 });
 
 app.post('/select-columns', (req, res) => {
     const { selectedColumns } = req.body; // Assuming selectedColumns is an array of column indices
     const fileBuffer = req.file.buffer.toString();
     const results = [];
-  
+
     // Process the CSV data (you can use csv-parser or other libraries for parsing)
     csv({ headers: true })
-      .toString(fileBuffer)
-      .on('data', (data) => results.push(data))
-      .on('end', () => {
-        // Extract selected columns
-        const selectedData = results.map((row) =>
-          selectedColumns.map((index) => row[index])
-        );
-  
-        // Send the selected columns data as JSON response
-        res.status(200).json(selectedData);
-      });
-  });  
+        .toString(fileBuffer)
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
+            // Extract selected columns
+            const selectedData = results.map((row) =>
+                selectedColumns.map((index) => row[index])
+            );
+
+            // Send the selected columns data as JSON response
+            res.status(200).json(selectedData);
+        });
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
